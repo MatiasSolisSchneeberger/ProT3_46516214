65cd7e59b0b8f0e580d45c4e4355b415373fa309
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2025-06-19 00:14:07 -0300

    creando base de datos

---
+++ app/Models/UserModel.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    protected $table = 'user';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['name', 'last_name', 'nickname', 'email', 'password', 'perfil_id', 'baja', 'created_at'];
+}
